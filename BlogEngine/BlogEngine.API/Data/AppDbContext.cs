0ed9288819cf2c5281f204a3dcc899ec6e8c6fd1
koss054 <konstantin054@abv.bg>
2023-01-20 17:38:55 +0200

    Create AppDbContext.cs

---
+++ BlogEngine/BlogEngine.API/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace BlogEngine.API.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Entities;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; } = null!;
+
+        public DbSet<Comment> Comments { get; set; } = null!;
+
+        public DbSet<User> User { get; set; } = null!;
+
+        public DbSet<UserBlog> UsersBlogs { get; set; } = null!;
+    }
+}
