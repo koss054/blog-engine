aef6aaeb2bfe2d0dc96e040eb964444c2a8a2cba
koss054 <konstantin054@abv.bg>
2023-01-20 17:42:31 +0200

    Update app to work with SqlLite

--- BlogEngine/BlogEngine.API/Program.cs
+++ BlogEngine/BlogEngine.API/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+using BlogEngine.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
