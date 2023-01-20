16bd776a3f28c9d03af9b3fa50d59c6bbdfe6ab2
koss054 <konstantin054@abv.bg>
2023-01-20 14:22:46 +0200

    Add initial changes for register functionality

--- BlogEngine/Program.cs
+++ BlogEngine/Program.cs
@@ -39,6 +39,15 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.MapRazorPages();
 
 app.Run();
