cbcfb08c4e4a6d0915782e4073b424d6cf835cd0
koss054 <konstantin054@abv.bg>
2023-01-20 22:18:32 +0200

    Create BlogsController.cs with test method

---
+++ BlogEngine/BlogEngine.API/Controllers/BlogsController.cs
@@ -0,0 +1,25 @@
+﻿namespace BlogEngine.API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using Data;
+    using Entities;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlogsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BlogsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Test()
+        {
+            return "testtttttt successful g";
+        } 
+    }
+}
