34feb15ad85c33df3c1c90a7f95da434cacfb23b
koss054 <konstantin054@abv.bg>
2023-01-20 22:18:06 +0200

    Add summary to entity props

--- BlogEngine/BlogEngine.API/Entities/Comment.cs
+++ BlogEngine/BlogEngine.API/Entities/Comment.cs
@@ -5,22 +5,40 @@
 
     public class Comment
     {
+        /// <summary>
+        /// Id of comment.
+        /// </summary>
         [Key]
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Content of comment.
+        /// </summary>
         [Required]
         [StringLength(300)]
         public string Content { get; set; } = null!;
 
+        /// <summary>
+        /// Id of comment author.
+        /// </summary>
         [ForeignKey(nameof(User))]
         public Guid UserId { get; set; }
 
+        /// <summary>
+        /// Comment author.
+        /// </summary>
         [Required]
         public User User { get; set; } = null!;
 
+        /// <summary>
+        /// Id of commented blog.
+        /// </summary>
         [ForeignKey(nameof(Blog))]
         public Guid BlogId { get; set; }
 
+        /// <summary>
+        /// Blog with comment.
+        /// </summary>
         [Required]
         public Blog Blog { get; set; } = null!;
     }
--- BlogEngine/BlogEngine.API/Entities/UserBlog.cs
+++ BlogEngine/BlogEngine.API/Entities/UserBlog.cs
@@ -5,15 +5,27 @@
 
     public class UserBlog
     {
+        /// <summary>
+        /// Id of user who is the blog author.
+        /// </summary>
         [ForeignKey(nameof(User))]
         public Guid UserId { get; set; }
 
+        /// <summary>
+        /// The blog author.
+        /// </summary>
         [Required]
         public User User { get; set; } = null!;
 
+        /// <summary>
+        /// Id of the blog made by the user.
+        /// </summary>
         [ForeignKey(nameof(Blog))]
         public Guid BlogId { get; set; }
 
+        /// <summary>
+        /// Blog written by the user.
+        /// </summary>
         [Required]
         public Blog Blog { get; set; } = null!;
     }
