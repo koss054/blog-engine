138c241db889390f80532a5b452f1b5c967d58f0
koss054 <konstantin054@abv.bg>
2023-02-03 21:30:54 +0200

    Make Angular page get API request

--- BlogEngine/client/src/app/app.component.ts
+++ BlogEngine/client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title: string = 'Blog Engine';
+  testString: any;
+  
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7055/api/blogs').subscribe({
+      next: response => this.testString = response,
+      error: () => console.log('error'),
+      complete: () => console.log('request completed') 
+    })
+  }
 }
